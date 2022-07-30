a60a897b58fb60dbfd0d13820548390cff3dde37
Daniel Cinto <dansolo.cc@gmail.com>
2022-07-29 21:53:09 -0500

    Custom design

--- app/src/main/java/mx/com/dcc/tutorialcompose/MainActivity.kt
+++ app/src/main/java/mx/com/dcc/tutorialcompose/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import mx.com.dcc.tutorialcompose.ui.theme.TutorialComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,13 +38,21 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.ic_android_black_24dp),
-            contentDescription = "Contact profile picture"
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp) // Set image size to 40 dp
+                .clip(CircleShape) // Clip image to be shaped as a circle.
         )
+        // Add a horizontal space between the image and the column.
+        Spacer(modifier = Modifier.width(8.dp))
         Column {
             Text(text = msg.author)
+            // Add vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
     }
